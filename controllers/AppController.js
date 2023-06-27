d0c8e14450b90a95b69305ea0280144e7fe52eff
Daniel Uboa <uboadaniel@gmail.com>
2023-06-27 21:08:12 +0300

    make new changes

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,23 +1,28 @@
-/* eslint-disable import/no-named-as-default */
-import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
-class AppController {
-  static async getStatus(req, res) {
-    const redisStatus = redisClient.isAlive();
-    const dbStatus = dbClient.isAlive();
-    res.status(200).json({
-      redis: redisStatus,
-      db: dbStatus,
-    });
-  }
-
-  static async getStats(req, res) {
-    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-      .then(([usersCount, filesCount]) => {
-        res.status(200).json({ users: usersCount, files: filesCount });
-      });
+/**
+ * Controller for endpoint GET /status that retrieves
+ * mongodb client and redis client connection status
+ * @typedef {import("express").Request} Request
+ * @typedef {import("express").Response} Response
+ * @param {Request} _req - request object
+ * @param {Response} res  - response object
+ */
+export function getStatus(_req, res) {
+  if (dbClient.isAlive() && redisClient.isAlive()) {
+    res.status(200).json({ redis: true, db: true });
   }
 }
 
-export default AppController;
+/**
+ * Controller for endpoint GET /stats that retrieves
+ * count of users and files
+ * @param {Request} _req - request object
+ * @param {Response} res  - response object
+ */
+export async function getStats(_req, res) {
+  const users = await dbClient.nbUsers();
+  const files = await dbClient.nbFiles();
+  res.status(200).json({ users, files });
+}
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import routes from './routes';
+import router from './routes/index';
 
+// Express server
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(routes);
-
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
+export default app;
